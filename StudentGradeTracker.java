ffaf18787040aaf1db655a229c98b12e9be03a1f
Aanya Sharma <aanya002sharma@gmail.com>
2026-08-01 20:25:04 +0530

    Rename class from CodeAlpha_StudentGradeTracker to StudentGradeTracker

--- StudentGradeTracker.java
+++ StudentGradeTracker.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class CodeAlpha_StudentGradeTracker {
+public class StudentGradeTracker {
 
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
